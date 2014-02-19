fce2bbf42ea9240cea902843ea9aa82b13285ce6
Peter Briers <peter.briers@gmail.com>
2014-02-19 15:50:38 +0100

    Improved meta viewport

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -50,7 +50,7 @@
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
